3a1673e3c9253a89d6431bde23d77cb44703aaf4
Arman Patel <vanixgaming634@gmail.com>
2023-03-15 13:44:01 +0530

    Not solvedM

---
+++ 50_find_x_divisors.c
@@ -0,0 +1,30 @@
+// C program to find number of integers which has exactly x divisors
+
+#include <stdio.h>
+
+
+int main () 
+{
+  int n=7, x=2;
+    
+    //Variable of count required numbers
+    int count = 0;
+    
+    for(int i=1; i<=n; i++){
+        
+        //variable to count the factors of i-th number
+        int count_factors = 0;
+        for(int j = 1; j<= i; j++){
+            if(i%j==0){
+                    count_factors++;
+            }
+        }
+        
+        if(count_factors == x)
+            count++;
+    }
+    
+    printf("%d ", count);
+
+ return 0;
+}
