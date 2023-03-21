50a4da52c124d622cc47fba3e9d236ee8878eec2
Arman Patel <vanixgaming634@gmail.com>
2023-03-21 15:00:50 +0530

    Not solved

--- 59_freqeuncy_of_element.c
+++ 59_freqeuncy_of_element.c
@@ -6,44 +6,26 @@
 void freq(int arr[], int len);
 int main()
 {
-    // int *num = NULL, size = 0, start = 0, end = 0;
+    int *num = NULL, size = 0, start = 0, end = 0;
 
-    // printf("Enter size of array: ");
-    // scanf("%d", &size);
+    printf("Enter size of array: ");
+    scanf("%d", &size);
   
 
-    // num = (int *)malloc(size * sizeof(int));
+    num = (int *)malloc(size * sizeof(int));
 
-    // printf("Enter elements of array:\n");
-    // for (int i = 0; i < size; i++)
-    // {
-    //     scanf("%d", &num[i]);
-    // }
+    printf("Enter elements of array:\n");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &num[i]);
+    }
 
-//    freq(num,size);
+   freq(num,size);
    
 
-    // free(num);
+    free(num);
 
-        int arr[] = {10, 30, 10, 20, 10, 20, 30, 10}; 
-    int n = sizeof(arr)/sizeof(arr[0]); 
-
-    int visited[n];
- 
-    for(int i=0; i<n; i++){
-
-       if(visited[i]==0){
-          int count = 1;
-          for(int j=i+1; j<n; j++){
-             if(arr[i]==arr[j]){
-                count++;
-                visited[j]=1;
-             }
-          }
-
-          printf("%d occurs %d times\n", arr[i], count);
-       }
-   }
+        
 
 
     return 0;
@@ -56,7 +38,7 @@ void freq(int arr[], int len)
 
    for(int i=0; i<len; i++){
 
-       if(visited[i]==0){
+      
           int count = 1;
           for(int j=i+1; j<len ; j++){
              if(arr[i]==arr[j]){
@@ -68,7 +50,6 @@ void freq(int arr[], int len)
           printf("%d occurs %d times\n", arr[i], count);
        }
     
-}
 }
 
 
